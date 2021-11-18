c285e6d32fd03d1b55178a277f67b82552ae6e15
Shane <shaneboo152@gmail.com>
2021-11-17 18:05:57 -0800

    fix

--- server/index.js
+++ server/index.js
@@ -1,42 +1,41 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express"); 
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const app = express()
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
-const postRoute = require("./routes/posts")
-require('dotenv').config()
-
-
+const app = express();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+require("dotenv").config();
 
 mongoose
-  .connect( process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("DB Connection Successfull"))
   .catch((err) => {
     console.error(err);
   });
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
-app.use(morgan("common"))
+app.use(morgan("common"));
 
+app.use("/api/users", userRoute);
 
-app.use("/api/users", userRoute )
+app.use("/api/auth", authRoute);
 
-app.use("/api/auth", authRoute )
-
-app.use("/api/posts", postRoute)
-
+app.use("/api/posts", postRoute);
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
-
-app.listen(process.env.PORT || 5000, ()=>{
-    console.log("backend server is running")
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("backend server is running");
+});
