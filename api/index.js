0909fc0bdcde900ecbdc4bcb31d32025f336d364
Shane <shaneboo152@gmail.com>
2021-11-18 18:05:30 -0800

    fix

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const app = express();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const path = require("path")
 require("dotenv").config();
 
 mongoose
